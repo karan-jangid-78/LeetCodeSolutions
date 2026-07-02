61344cd4f69c71b974fa4c6f30382c5f76d5333c
karan-jangid-78 <karan9664jangid@gmail.com>
2026-07-02 09:49:39 +0530

    Time: 0 ms (100%), Space: 26 MB (57.4%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,16 +1,16 @@
 class Solution {
-public:
+public://an optmized sol..it uses two pointer approach and the thought process comes by that we neend only min from lmax,rmax so we need only one either lmax or rmax 
     int trap(vector<int>& height) {
         int n=height.size();
         int l=0;
         int r=n-1;
         int lmax=0,rmax=0,total=0;
         while(l<r){
-            if(height[l]<height[r]){
-                if(lmax>height[l]){
+            if(height[l]<height[r]){// it means there is larger building in right and then we only look for lmax
+                if(lmax>height[l]){//it means water can be trapped 
                     total+=lmax-height[l];
                 }
-                else{
+                else{//it means it is itself greater than lmax then we update lmax
                     lmax=height[l];
                 }
                 l++;
