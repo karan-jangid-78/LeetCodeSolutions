7f7e9971de2e0d566b9304a2edc74029edde2de8
karan-jangid-78 <karan9664jangid@gmail.com>
2026-07-07 15:06:00 +0530

    Time: 20 ms (64.64%), Space: 81.3 MB (72.46%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -3,6 +3,8 @@ public:
     int largestRectangleArea(vector<int>& heights) {
         int n=heights.size();
         stack<int> st;
+        // optimized version because we have to calculate nse and pse separately and then iterate through whole array but in this at once a time i calculate nse and pse simaltneously..
+        // the intuition is whenever we pop we can calculate the nse and pse for the element which have to be popped
         int ans=INT_MIN;
         for(int i=0;i<n;i++){
             while(!st.empty()&&heights[i]<heights[st.top()]){
