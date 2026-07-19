5c6aefe0d69b5380433066b15dc9163e4c6577b2
karan-jangid-78 <karan9664jangid@gmail.com>
2026-07-19 20:51:43 +0530

    Time: 0 ms (100%), Space: 12.7 MB (40.39%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -11,7 +11,7 @@ public:
             if(nums[mid]>nums[mid+1]&&nums[mid]>nums[mid-1]){
                 return mid;
             }
-            else if(nums[mid]>nums[mid-1]){
+            if(nums[mid]>nums[mid-1]){
                 low=mid+1;
             }
             else if(nums[mid]>nums[mid+1]){
