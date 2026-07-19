1aeac06d31b7f08649d88f1a9c3fa347607611bb
karan-jangid-78 <karan9664jangid@gmail.com>
2026-07-19 17:45:32 +0530

    Time: 0 ms (100%), Space: 26.2 MB (35.31%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -9,10 +9,17 @@ public:
         while(low<=high){
             int mid=low+(high-low)/2;
             if(nums[mid]!=nums[mid-1]&&nums[mid]!=nums[mid+1]){return nums[mid];}
-           if((mid%2==1&&nums[mid]==nums[mid-1])||(mid%2==0&&nums[mid]==nums[mid+1])){
+           if(mid%2==1){
+                 if(nums[mid]==nums[mid-1]){              
+           low=mid+1;}
+           else{high=mid-1;}
+           }
+           else{
+            if(nums[mid]==nums[mid+1]){
             low=mid+1;
            }
            else{high=mid-1;}
+           }
         }
         return -1;
     }
