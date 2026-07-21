411563200521849ea75eba6ab5747238bd01affd
karan-jangid-78 <karan9664jangid@gmail.com>
2026-07-21 11:12:52 +0530

    Time: 0 ms (100%), Space: 10.6 MB (43.01%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-bool solve(vector<int>& nums, int k,int pages){
-    int nos=1;
-    int sp=nums[0];
-    for(int i=1;i<nums.size();i++){
-        if(nums[i]+sp<=pages){
-            sp+=nums[i];
+bool solve(vector<int>& nums, int k,int capacity){
+    int nop=1;
+    int carryingthecapacity=0;
+    for(int i=0;i<nums.size();i++){
+        if(nums[i]+carryingthecapacity<=capacity){
+            carryingthecapacity+=nums[i];
         }
         else{
-            nos++;
-            sp=nums[i];
+            carryingthecapacity=nums[i];
+            nop++;
         }
     }
-    return nos<=k;
+    return nop<=k;
 }
     int splitArray(vector<int>& nums, int k) {
-        if(k>nums.size()) return -1;
-        int max=*max_element(nums.begin(),nums.end());
-        int sum=accumulate(nums.begin(),nums.end(),0);
-        int low=max,high=sum;
+        int low=*max_element(nums.begin(),nums.end());
+        int high=accumulate(nums.begin(),nums.end(),0);
         while(low<=high){
             int mid=low+(high-low)/2;
             if(solve(nums,k,mid)==true){
@@ -29,6 +27,5 @@ bool solve(vector<int>& nums, int k,int pages){
             }
         }
         return low;
-        
     }
 };
