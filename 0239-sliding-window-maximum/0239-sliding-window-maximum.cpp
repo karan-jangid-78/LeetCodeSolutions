2f64ff79b27e9cc218d451b6ef4bef201b4e65f8
karan-jangid-78 <karan9664jangid@gmail.com>
2026-07-24 00:16:10 +0530

    Time: 23 ms (57.82%), Space: 139.2 MB (69.66%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        //deque is more optimal than using map
         deque<int> dq;
         vector<int> ans;
         for(int i=0;i<nums.size();i++){
@@ -18,3 +19,24 @@ public:
         return ans;
     }
 };
+// the code using map is class Solution {
+// public:
+//     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+//         int n=nums.size();
+//         map<int,int> mp;
+//         vector<int> ans;
+//         for(int i=0;i<n;i++){
+//             mp[nums[i]]++;
+//             if(i>=k){
+//                 mp[nums[i-k]]--;
+//                 if(mp[nums[i-k]]==0){
+//                     mp.erase(nums[i-k]);
+//                 }
+//             }
+//             if(i>=k-1){
+//                 ans.push_back(mp.rbegin()->first);
+//             }
+//         }
+//         return ans;
+//     }
+// };
