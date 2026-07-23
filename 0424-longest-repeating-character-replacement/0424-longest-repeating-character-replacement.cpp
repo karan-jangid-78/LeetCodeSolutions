640edd9c2627bd612d5d7cac89bbfa8a1718dde6
karan-jangid-78 <karan9664jangid@gmail.com>
2026-07-23 16:58:46 +0530

    Time: 6 ms (47.81%), Space: 10.8 MB (93.57%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -8,13 +8,16 @@ public:
         int maxfreq=0;
         while(r<n){
             hash[s[r]-'A']++;
+            //if the maxlen=5 and mf=3 then 5-3=2 is the last valid condition and if i want a better ans maxlen=6 then 6-3 will also invalid then there is no point to keep the mf=2 because 6-2 will also invalid if maxlen has to be 6 then mf should be 4 it means mf should increase for better maxlen
             maxfreq=max(maxfreq,hash[s[r]-'A']);
+            //second optimization using if instead of while
             if((r-l+1)-maxfreq>k){
                 hash[s[l]-'A']--;
                 maxfreq=0;
-                for(int i=0;i<26;i++){
-                    maxfreq=max(maxfreq,hash[i]);
-                }
+                //first optimization 
+                // for(int i=0;i<26;i++){
+                //     maxfreq=max(maxfreq,hash[i]);
+                // }
                 l++;
             }
             ans=max(ans,r-l+1);
