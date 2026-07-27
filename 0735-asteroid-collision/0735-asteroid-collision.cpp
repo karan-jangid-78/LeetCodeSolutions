a11d6c38cc3be7fc81ad724ed93298a8fd5f36cc
karan-jangid-78 <karan9664jangid@gmail.com>
2026-07-27 12:53:36 +0530

    Time: 3 ms (28.36%), Space: 22.3 MB (6.48%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -5,9 +5,7 @@ public:
         stack<int> st;
         vector<int> ans;
         for(int i=0;i<n;i++){
-            if(asteroids[i]>0){
-                st.push(asteroids[i]);
-            }
+            if(asteroids[i]>0) st.push(asteroids[i]);
             else{
                 while(!st.empty()&&st.top()>0&&abs(asteroids[i])>st.top()){
                     st.pop();
@@ -15,9 +13,7 @@ public:
                 if(!st.empty()&&abs(asteroids[i])==st.top()){
                     st.pop();
                 }
-                else if(st.empty()||st.top()<0){
-                    st.push(asteroids[i]);
-                }
+               else if(st.empty()||st.top()<0) st.push(asteroids[i]);
             }
         }
         while(!st.empty()){
