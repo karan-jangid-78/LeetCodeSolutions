dc9d4732279170c6a7ca620c56694f5efc825033
karan-jangid-78 <karan9664jangid@gmail.com>
2026-08-02 13:17:09 +0530

    Time: 0 ms (100%), Space: 50.5 MB (8.59%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-int indexofmaxelement(vector<vector<int>>& mat,int n,int m,int col){
-    int index=-1;
+int indexofmaxelement(vector<vector<int>>&mat,int n,int m,int col){
+    int index=0;
     int maxi=INT_MIN;
     for(int i=0;i<n;i++){
-        if(mat[i][col]>maxi){
-        maxi=max(maxi,mat[i][col]);
-        index=i;
+        if(mat[i][col]>=maxi){
+            maxi=mat[i][col];
+            index=i;
         }
     }
     return index;
@@ -21,7 +21,7 @@ int indexofmaxelement(vector<vector<int>>& mat,int n,int m,int col){
             int left=mid-1>=0?mat[row][mid-1]:-1;
             int right=mid+1<m?mat[row][mid+1]:-1;
             if(mat[row][mid]>left&&mat[row][mid]>right) return {row,mid};
-            else if(mat[row][mid]<left) high=mid-1;
+            else if(left>mat[row][mid]) high=mid-1;
             else low=mid+1;
         }
         return {-1,-1};
