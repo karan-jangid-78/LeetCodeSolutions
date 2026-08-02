7770bdbe1b0ea598a6d5b6a75c28c2b71680a34c
karan-jangid-78 <karan9664jangid@gmail.com>
2026-08-02 21:54:50 +0530

    Time: 2 ms (39.71%), Space: 10.7 MB (40.55%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -4,7 +4,7 @@ vector<vector<int>> result;
 vector<int> ans;
 void solve(vector<int>& nums,int index){
     result.push_back(ans);//because whenever a function is called i'm sure that it comes after skipping   //the duplicats and pushed into ans
-    for(int i=index;i<nums.size();i++){
+    for(int i=index;i<nums.size();i++){//for loop is used to explore all the possible choices
         if(i>index&&nums[i]==nums[i-1]) continue;//index ==from where function starts to making choice so //that when i backtrack then i can have this information that i can start from here to make choice
         ans.push_back(nums[i]);
         solve(nums,i+1);
