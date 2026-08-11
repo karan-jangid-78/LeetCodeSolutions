cf1532ef0b3768295ea793e7719952860fe67267
karan-jangid-78 <karan9664jangid@gmail.com>
2026-08-11 23:54:24 +0530

    Time: 14 ms (34.69%), Space: 35 MB (89.11%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
-bool solve(vector<int>& weights, int days,int capacity){
-    int count=0;
-    int nod=1;
-    for(int i=0;i<weights.size();i++){
-        if(count+weights[i]<=capacity){
-           
-            count+=weights[i];
-        }
-        else{
-            nod++;
-            count=weights[i];
-        }
+int solve(int capacity,vector<int>& weights){
+    int cnt=1;
+    int n=weights.size();
+    int sum=0;
+    for(int i=0;i<n;i++){
+        sum+=weights[i];
+        if(sum>capacity){ cnt++; sum=weights[i];} 
     }
-    return nod<=days;
+    return cnt;
 }
     int shipWithinDays(vector<int>& weights, int days) {
-        int sum=accumulate(weights.begin(),weights.end(),0);
-        int max=*max_element(weights.begin(),weights.end());
-        int low=max,high=sum;
+        int n=weights.size();
+        int sum=0;
+        int maxi=INT_MIN;
+        for(int i=0;i<n;i++){
+            sum+=weights[i];
+            if(weights[i]>maxi) maxi=weights[i];
+        }
+        int low=maxi,high=sum;
         while(low<=high){
             int mid=low+(high-low)/2;
-            if(solve(weights,days,mid)==true){
-                high=mid-1;
-            }
-            else{
-                low=mid+1;
-            }
+            int no_of_days=solve(mid,weights);
+            if(no_of_days>days) low=mid+1;
+            else high=mid-1;
         }
         return low;
     }
