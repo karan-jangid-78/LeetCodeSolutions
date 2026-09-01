beef3d0d62de6b34bf33caad4b1b1c351333472f
karan-jangid-78 <karan9664jangid@gmail.com>
2026-09-01 15:18:12 +0530

    Time: 399 ms (46.76%), Space: 211.3 MB (51.36%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-int func(int index,int buy,vector<int>& prices,int no_of_transactions,vector<vector<vector<int>>> &dp){
-    if(index==prices.size()) return 0;
-    if(no_of_transactions==0) return 0;
-    int profit=0;
-    if(dp[index][buy][no_of_transactions]!=-1) return dp[index][buy][no_of_transactions];
-    if(buy==1){
-        profit=max(-prices[index]+func(index+1,0,prices,no_of_transactions,dp),//take
-        func(index+1,1,prices,no_of_transactions,dp));//not take
-    }
-    else{
-        profit=max(prices[index]+func(index+1,1,prices,no_of_transactions-1,dp),//take
-        func(index+1,0,prices,no_of_transactions,dp));//nottake
-    }
-    return dp[index][buy][no_of_transactions]=profit;
-}
-
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<vector<int>>> dp(n,
-                     vector<vector<int>>(2,vector<int>(3,-1)));
-        return func(0,1,prices,2,dp);
+        vector<vector<vector<int>>> dp(n+1,
+                     vector<vector<int>>(2,vector<int>(3,0)));
+        for(int index=n-1;index>=0;index--){
+            for(int buy=0;buy<2;buy++){
+                for(int no_of_transactions=1;no_of_transactions<=2;no_of_transactions++){
+                    int profit=0;
+                    if(buy==1){
+                        profit=max(-prices[index]+dp[index+1][0][no_of_transactions],//take
+                        dp[index+1][1][no_of_transactions]);//not take
+                    }
+                    else{
+                        profit=max(prices[index]+dp[index+1][1][no_of_transactions-1],//take
+                        dp[index+1][0][no_of_transactions]);//nottake
+                    }
+                     dp[index][buy][no_of_transactions]=profit;
+                }
+            }
+        }
+        return dp[0][1][2];
     }
 };
