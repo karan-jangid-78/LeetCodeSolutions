b796c09db32bee0ec681f84c4f116f2bca2614d4
karan-jangid-78 <karan9664jangid@gmail.com>
2026-09-01 16:11:36 +0530

    Time: 144 ms (59.57%), Space: 139.7 MB (56.51%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-int func(int index,int tranNo,vector<int>& prices,vector<vector<int>> &dp){
-    if(tranNo>=4||index==prices.size()) return 0;
-    int profit=0;
-    if(dp[index][tranNo]!=-1) return dp[index][tranNo];
-    if(tranNo%2==0){
-        profit=max(-prices[index]+func(index+1,tranNo+1,prices,dp),//take
-        func(index+1,tranNo,prices,dp));
-        }//not take
-    else{
-        profit=max(prices[index]+func(index+1,tranNo+1,prices,dp),//take
-        func(index+1,tranNo,prices,dp));//nottake
-    }
-    return dp[index][tranNo]=profit;
-}
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<int>> dp(n+1,vector<int>(4,-1));
-        return func(0,0,prices,dp);
+        vector<vector<int>> dp(n+1,vector<int>(4+1,0));
+        for(int index=n-1;index>=0;index--){
+            for(int tranNo=3;tranNo>=0;tranNo--){
+                int profit=0;
+                if(tranNo%2==0){
+                    profit=max(-prices[index]+dp[index+1][tranNo+1],//take
+                    dp[index+1][tranNo]);
+                    }//not take
+                else{
+                    profit=max(prices[index]+dp[index+1][tranNo+1],//take
+                    dp[index+1][tranNo]);//nottake
+                }
+                 dp[index][tranNo]=profit;
+            }
+        }
+        return dp[0][0];
     }
 };
