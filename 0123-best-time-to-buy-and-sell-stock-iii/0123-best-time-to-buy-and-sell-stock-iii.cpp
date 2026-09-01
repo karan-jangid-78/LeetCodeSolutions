defed60c83a6eea22849ced9ea83d66e7c657149
karan-jangid-78 <karan9664jangid@gmail.com>
2026-09-01 15:21:27 +0530

    Time: 51 ms (70.35%), Space: 79.4 MB (79.76%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -2,24 +2,25 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<vector<int>>> dp(n+1,
-                     vector<vector<int>>(2,vector<int>(3,0)));
+        vector<vector<int>> after(2,vector<int>(3,0));
+        vector<vector<int>> cur(2,vector<int>(3,0));
         for(int index=n-1;index>=0;index--){
             for(int buy=0;buy<2;buy++){
                 for(int no_of_transactions=1;no_of_transactions<=2;no_of_transactions++){
                     int profit=0;
                     if(buy==1){
-                        profit=max(-prices[index]+dp[index+1][0][no_of_transactions],//take
-                        dp[index+1][1][no_of_transactions]);//not take
+                        profit=max(-prices[index]+after[0][no_of_transactions],//take
+                        after[1][no_of_transactions]);//not take
                     }
                     else{
-                        profit=max(prices[index]+dp[index+1][1][no_of_transactions-1],//take
-                        dp[index+1][0][no_of_transactions]);//nottake
+                        profit=max(prices[index]+after[1][no_of_transactions-1],//take
+                        after[0][no_of_transactions]);//nottake
                     }
-                     dp[index][buy][no_of_transactions]=profit;
+                     cur[buy][no_of_transactions]=profit;
                 }
             }
+            after=cur;
         }
-        return dp[0][1][2];
+        return after[1][2];
     }
 };
