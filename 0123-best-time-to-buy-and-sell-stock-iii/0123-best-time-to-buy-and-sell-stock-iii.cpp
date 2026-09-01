1c911b42bc88e72b37c9a57a32e4d3d0ccd730b5
karan-jangid-78 <karan9664jangid@gmail.com>
2026-09-01 15:59:48 +0530

    Time: 171 ms (57.56%), Space: 137 MB (60.2%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
+int func(int index,int tranNo,vector<int>& prices,vector<vector<int>> &dp){
+    if(tranNo>=4||index==prices.size()) return 0;
+    int profit=0;
+    if(dp[index][tranNo]!=-1) return dp[index][tranNo];
+    if(tranNo%2==0){
+        profit=max(-prices[index]+func(index+1,tranNo+1,prices,dp),//take
+        func(index+1,tranNo,prices,dp));
+        }//not take
+    else{
+        profit=max(prices[index]+func(index+1,tranNo+1,prices,dp),//take
+        func(index+1,tranNo,prices,dp));//nottake
+    }
+    return dp[index][tranNo]=profit;
+}
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<int>> after(2,vector<int>(3,0));
-        vector<vector<int>> cur(2,vector<int>(3,0));
-        for(int index=n-1;index>=0;index--){
-            for(int buy=0;buy<2;buy++){
-                for(int no_of_transactions=1;no_of_transactions<=2;no_of_transactions++){
-                    int profit=0;
-                    if(buy==1){
-                        profit=max(-prices[index]+after[0][no_of_transactions],//take
-                        after[1][no_of_transactions]);//not take
-                    }
-                    else{
-                        profit=max(prices[index]+after[1][no_of_transactions-1],//take
-                        after[0][no_of_transactions]);//nottake
-                    }
-                     cur[buy][no_of_transactions]=profit;
-                }
-            }
-            after=cur;
-        }
-        return after[1][2];
+        vector<vector<int>> dp(n+1,vector<int>(4,-1));
+        return func(0,0,prices,dp);
     }
 };
