929fa4e4d3590958fe44078b35eb6f0de054c13f
karan-jangid-78 <karan9664jangid@gmail.com>
2026-09-01 16:18:04 +0530

    Time: 4 ms (18.49%), Space: 16.3 MB (22.79%) - LeetHub

---
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+int func(int index,int buy,vector<int>& prices,vector<vector<int>>& dp){
+    if(index>=prices.size()) return 0;
+    int profit=0;
+    if(dp[index][buy]!=-1) return dp[index][buy];
+    if(buy==1){
+        profit=max(-prices[index]+func(index+1,0,prices,dp),//take
+        func(index+1,1,prices,dp));//not take
+    }
+    else{
+        //index==n-1 and you are acccessing index+2
+        profit=max(prices[index]+func(index+2,1,prices,dp),//take
+        func(index+1,0,prices,dp));//nottake
+    }
+    return dp[index][buy]=profit;
+}
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        vector<vector<int>> dp(n,vector<int>(2,-1));
+        return func(0,1,prices,dp);
+    }
+};
