d2503955cfa811f1cb68f74ab00fcb27d5b20ad9
karan-jangid-78 <karan9664jangid@gmail.com>
2026-09-08 17:09:24 +0530

    Time: 90 ms (89.12%), Space: 13.9 MB (30.8%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-int func(int i,int j,vector<int> &nums,vector<vector<int>> &dp){
-    if(i>j) return 0;
-    int maxi=0;
-    if(dp[i][j]!=-1) return dp[i][j];
-    for(int k=i;k<=j;k++){
-        int coins=nums[i-1]*nums[k]*nums[j+1]+func(i,k-1,nums,dp)+func(k+1,j,nums,dp);
-        maxi=max(maxi,coins);
-    }
-    return dp[i][j]=maxi;
-}
     int maxCoins(vector<int>& nums) {
         int n=nums.size();
         nums.push_back(1);
         nums.insert(nums.begin(),1);
-        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-        return func(1,n,nums,dp);
+        vector<vector<int>> dp(n+2,vector<int>(n+2,0));
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=n;j++){
+                if(i>j) continue;
+                int maxi=0;
+                for(int k=i;k<=j;k++){
+                    int coins=nums[i-1]*nums[k]*nums[j+1]+dp[i][k-1]+dp[k+1][j];
+                    maxi=max(maxi,coins);
+                }
+                dp[i][j]=maxi;
+            }
+        }
+        return dp[1][n];
     }
 };
