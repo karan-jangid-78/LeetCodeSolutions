b05c92ede0d7897cb2d8d52d3d6e81bbbeef2bac
karan-jangid-78 <karan9664jangid@gmail.com>
2026-09-09 12:36:30 +0530

    Time: 1513 ms (31.88%), Space: 9.2 MB (87.98%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -8,22 +8,19 @@ bool ispalindrome(int i,int j,string &s){
     }
     return true;
 }
-int func(int i,string &s,vector<int>& dp){
-    int n=s.size();
-    if(i==n) return 0;
-    int mincost=INT_MAX;
-    if(dp[i]!=-1) return dp[i];
-    for(int j=i;j<=n-1;j++){
-        if(ispalindrome(i,j,s)){
-            int cost =1+func(j+1,s,dp);
-             mincost=min(mincost,cost);
-        }
-    }
-    return dp[i]=mincost;
-}
     int minCut(string s) {
         int n=s.size();
-        vector<int> dp(n,-1);
-        return func(0,s,dp)-1;
+        vector<int> dp(n+1,0);
+        for(int i=n-1;i>=0;i--){
+            int mincost=INT_MAX;
+            for(int j=i;j<=n-1;j++){
+                if(ispalindrome(i,j,s)){
+                    int cost =1+dp[j+1];
+                    mincost=min(mincost,cost);
+                }
+            }
+             dp[i]=mincost;
+        }
+        return dp[0]-1;
     }
 };
