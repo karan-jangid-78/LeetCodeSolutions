12cd30c542e6765154e1724871356de06a5347c9
karan-jangid-78 <karan9664jangid@gmail.com>
2026-09-12 17:05:32 +0530

    Time: 3 ms (91.46%), Space: 10.3 MB (96.24%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -3,20 +3,22 @@ public:
     int minDistance(string word1, string word2) {
         int m=word1.size();
         int n=word2.size();
-        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
-        for(int i=0;i<=m;i++) dp[i][0]=i;
-        for(int j=1;j<=n;j++) dp[0][j]=j;
+        vector<int> prev(n+1,0), cur(n+1,0);
+        for(int i=0;i<=m;i++) cur[0]=i;
+        for(int j=1;j<=n;j++) prev[j]=j;
         for(int i=1;i<=m;i++){
+            cur[0]=i;
             for(int j=1;j<=n;j++){
-                if(word1[i-1]==word2[j-1])  dp[i][j]=dp[i-1][j-1];
+                if(word1[i-1]==word2[j-1])  cur[j]=prev[j-1];
                 else{
-                int insert=1+dp[i][j-1];
-                int deletee=1+dp[i-1][j];
-                int replace=1+dp[i-1][j-1];
-                 dp[i][j]=min({insert,deletee,replace});
+                int insert=1+cur[j-1];
+                int deletee=1+prev[j];
+                int replace=1+prev[j-1];
+                 cur[j]=min({insert,deletee,replace});
                 }
             }
+            prev=cur;
         }
-        return dp[m][n];
+        return prev[n];
     }
 };
