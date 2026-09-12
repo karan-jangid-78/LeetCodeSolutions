2a65d5d10afaeec2bff02e7b4fcbe9d19ee35808
karan-jangid-78 <karan9664jangid@gmail.com>
2026-09-12 16:59:36 +0530

    Time: 8 ms (45.38%), Space: 13.5 MB (24.85%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-int func(int i,int j,string &word1,string &word2,vector<vector<int>>& dp){
-    if(j<0) return i+1;
-    if(i<0) return j+1;
-    if(dp[i][j]!=-1) return dp[i][j];
-    if(word1[i]==word2[j]) return dp[i][j]=func(i-1,j-1,word1,word2,dp);
-    else{
-    int insert=1+func(i,j-1,word1,word2,dp);
-    int deletee=1+func(i-1,j,word1,word2,dp);
-    int replace=1+func(i-1,j-1,word1,word2,dp);
-    return dp[i][j]=min({insert,deletee,replace});
-    }
-}
     int minDistance(string word1, string word2) {
         int m=word1.size();
         int n=word2.size();
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return func(m-1,n-1,word1,word2,dp);
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        for(int i=0;i<=m;i++) dp[i][0]=i;
+        for(int j=1;j<=n;j++) dp[0][j]=j;
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(word1[i-1]==word2[j-1])  dp[i][j]=dp[i-1][j-1];
+                else{
+                int insert=1+dp[i][j-1];
+                int deletee=1+dp[i-1][j];
+                int replace=1+dp[i-1][j-1];
+                 dp[i][j]=min({insert,deletee,replace});
+                }
+            }
+        }
+        return dp[m][n];
     }
 };
