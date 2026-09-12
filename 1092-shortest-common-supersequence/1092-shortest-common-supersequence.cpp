613811ddbdad24d3d457b6486189000a591333a7
karan-jangid-78 <karan9664jangid@gmail.com>
2026-09-12 16:18:32 +0530

    Time: 22 ms (91.96%), Space: 27.1 MB (11.27%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     string shortestCommonSupersequence(string str1, string str2) {
-        int n=str1.size();
-        int m=str2.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        for(int j=0;j<=m;j++) dp[0][j]=0;
-        for(int i=0;i<=n;i++) dp[i][0]=0;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(str1[i-1]==str2[j-1]) dp[i][j]=1+dp[i-1][j-1];
-                else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-            }
+          int m=str1.size();
+          int n=str2.size();
+          vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+          for(int i=1;i<=m;i++){
+              for(int j=1;j<=n;j++){
+                  if(str1[i-1]==str2[j-1]) {
+             dp[i][j]=1+dp[i-1][j-1];
         }
-        string ans="";
-        int i=n;
-        int j=m;
-        while(i>0&&j>0){
+          else dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
+              }
+          }
+          int i=m,j=n;
+          string ans="";
+          while(i>0 && j>0){
             if(str1[i-1]==str2[j-1]){
                 ans+=str1[i-1];
                 i--;
@@ -28,21 +27,20 @@ public:
                 }
                 else{
                     ans+=str1[i-1];
-                    i--;          
-                  }
+                    i--;
+                }
             }
-        }
-        //exhausted cases
-        while(i>0){
+          }
+          while(i>0){
             ans+=str1[i-1];
             i--;
-        }
-        while(j>0){
+          }
+          while(j>0){
             ans+=str2[j-1];
             j--;
-        }
-        string temp=ans;
-        reverse(temp.begin(),temp.end());
-        return temp;
+          }
+          string temp=ans;
+          reverse(temp.begin(),temp.end());
+          return temp;
     }
 };
