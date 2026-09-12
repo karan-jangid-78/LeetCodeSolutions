bfdbffc61c84346d6d3f5c29da42ca68084d7c88
karan-jangid-78 <karan9664jangid@gmail.com>
2026-09-12 18:30:13 +0530

    Time: 116 ms (16.64%), Space: 53.7 MB (40.67%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,39 +1,29 @@
 class Solution {
 public:
-// bool func(int i, int j, string& s1,string& s2,vector<vector<int>>& dp){
-//     if(i<0 && j<0) return true;
-//     if(i<0 && j>=0) return false;
-//     if(j==0 && i>0){
-//         for(int ind=i;ind>0;ind--){
-//             if(s1[ind]!='*') return false;
-//         }
-//         return true;
-//     }
-//     if(dp[i][j]!=-1) return dp[i][j];
-//     if(s1[i]==s2[j] || s1[i]=='?') return dp[i][j]=func(i-1,j-1,s1,s2,dp);
-//     if(s1[i]=='*') return dp[i][j]=func(i-1,j,s1,s2,dp)||func(i,j-1,s1,s2,dp);
-//     return dp[i][j]=false;
-// }
+bool func(int i, int j, string& s1,string& s2,vector<vector<int>>& dp){
+    if(i<0 && j<0) return true;
+    if(i<0 && j>=0) return false;
+    if(j<0 && i>=0){
+        for(int ind=i;ind>=0;ind--){
+            if(s1[ind]!='*') return false;
+        }
+        return true;
+    }
+    if(dp[i][j]!=-1) return dp[i][j];
+    if(s1[i]==s2[j] || s1[i]=='?') return dp[i][j]=func(i-1,j-1,s1,s2,dp);
+    if(s1[i]=='*'){//naive approach
+        for(int k=0;k<=j+1;k++){
+            if(func(i-1,j-k,s1,s2,dp)==true) return dp[i][j]=true;
+
+        }
+        return dp[i][j]=false;
+    }
+    return dp[i][j]=false;
+}
     bool isMatch(string s, string p) {
         int m=p.size();
         int n=s.size();
-        vector<vector<bool>> dp(m+1,vector<bool>(n+1,false));
-        dp[0][0]=true;
-        for(int j=1;j<=n;j++) dp[0][j]=false;
-        for(int i=1;i<=m;i++){
-            bool flag=true;
-                for(int ind=1;ind<=i;ind++){
-                    if(p[ind-1]!='*') {flag=false; break;}
-                }
-                 dp[i][0]=flag;
-        }
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
-                    if(p[i-1]==s[j-1] || p[i-1]=='?')  dp[i][j]=dp[i-1][j-1];
-                    else if(p[i-1]=='*')  dp[i][j]=dp[i-1][j]||dp[i][j-1];
-                    else dp[i][j]=false;
-            }
-        }
-        return dp[m][n];
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return func(m-1,n-1,p,s,dp);
     }
 };
