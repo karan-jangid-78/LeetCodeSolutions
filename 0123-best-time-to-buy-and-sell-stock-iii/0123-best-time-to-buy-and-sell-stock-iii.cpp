ae268ad99276eada5e56fe1a80655e80017d9ffe
karan-jangid-78 <karan9664jangid@gmail.com>
2026-09-14 16:54:41 +0530

    Time: 179 ms (55.47%), Space: 150.5 MB (53.84%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-int func(int i,int buy,int tranNo,vector<int> &prices,vector<vector<vector<int>>> &dp){
+int func(int i,int tranNo,vector<int> &prices,vector<vector<int>> &dp){
     int n=prices.size();
-    if(tranNo==0) return 0;
+    if(tranNo==4) return 0;
     if(i==n) return 0;
-    if(dp[i][buy][tranNo]!=-1) return dp[i][buy][tranNo];
-    if(buy==1){
-        return dp[i][buy][tranNo]=max(-prices[i]+func(i+1,0,tranNo,prices,dp),0+func(i+1,1,tranNo,prices,dp));
+    if(dp[i][tranNo]!=-1) return dp[i][tranNo];
+    if(tranNo%2==0){
+        return dp[i][tranNo]=max(-prices[i]+func(i+1,tranNo+1,prices,dp),0+func(i+1,tranNo,prices,dp));
     }
     else{
-        return dp[i][buy][tranNo]=max(+prices[i]+func(i+1,1,tranNo-1,prices,dp),0+func(i+1,0,tranNo,prices,dp));
+        return dp[i][tranNo]=max(+prices[i]+func(i+1,tranNo+1,prices,dp),0+func(i+1,tranNo,prices,dp));
     }
 }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<vector<int>>> dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
-        return func(0,1,2,prices,dp);
+        vector<vector<int>> dp(n,vector<int>(5,-1));
+        return func(0,0,prices,dp);
     }
 };
