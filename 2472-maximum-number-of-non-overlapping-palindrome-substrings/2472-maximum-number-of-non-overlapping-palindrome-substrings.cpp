ac142a3861790ac92cb9001dc584e9967f4b23cd
karan-jangid-78 <karan9664jangid@gmail.com>
2026-09-15 17:51:07 +0530

    Time: 0 ms (100%), Space: 9.7 MB (76.44%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -8,19 +8,17 @@ bool ispalindrome(int i,int j,string &s){
     }
     return true;
 }
-int func(int i,int k,string &s,vector<int> &dp){
-    int n=s.size();
-    if(i>=n) return 0;
-    if(dp[i]!=-1) return dp[i];
-    int nottake=func(i+1,k,s,dp);
-    int take1=-1e9,take2=-1e9;
-     if(i+k<=n && ispalindrome(i,i+k-1,s))  take1=1+func(i+k,k,s,dp);
-    if(i+k+1<=n && ispalindrome(i,i+k,s))  take2=1+func(i+k+1,k,s,dp);
-    return dp[i]=max({nottake,take1,take2});
-}
+
     int maxPalindromes(string s, int k) {
         int n=s.size();
-        vector<int> dp(n,-1);
-        return func(0,k,s,dp);
+        vector<int> dp(n+k,0);
+        for(int i=n-1;i>=0;i--){
+            int nottake=dp[i+1];
+            int take1=-1e9,take2=-1e9;
+            if(i+k<=n && ispalindrome(i,i+k-1,s))  take1=1+dp[i+k];
+            if(i+k+1<=n && ispalindrome(i,i+k,s))  take2=1+dp[i+k+1];
+             dp[i]=max({nottake,take1,take2});
+        }
+        return dp[0];
     }
 };
