cc1bec0bd0349d3166553afe80fbe902d3107509
karan-jangid-78 <karan9664jangid@gmail.com>
2026-09-15 17:47:56 +0530

    Time: 0 ms (100%), Space: 10.9 MB (69.11%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -13,14 +13,10 @@ int func(int i,int k,string &s,vector<int> &dp){
     if(i>=n) return 0;
     if(dp[i]!=-1) return dp[i];
     int nottake=func(i+1,k,s,dp);
-    int maxi=-1e9;
-    for(int len=k;i+len<=n;len++){
-        if(ispalindrome(i,i+len-1,s)==true){
-           int take=1+func(i+len,k,s,dp);
-            maxi=max(maxi,take);
-        }
-    }
-    return dp[i]=max(maxi,nottake);
+    int take1=-1e9,take2=-1e9;
+     if(i+k<=n && ispalindrome(i,i+k-1,s))  take1=1+func(i+k,k,s,dp);
+    if(i+k+1<=n && ispalindrome(i,i+k,s))  take2=1+func(i+k+1,k,s,dp);
+    return dp[i]=max({nottake,take1,take2});
 }
     int maxPalindromes(string s, int k) {
         int n=s.size();
