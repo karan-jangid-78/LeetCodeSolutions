ea73ee0d8bf03b48c0fe1dcba252892e8b44f98e
karan-jangid-78 <karan9664jangid@gmail.com>
2026-09-24 16:07:51 +0530

    Time: 0 ms (100%), Space: 10.7 MB (61.41%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
-int func(int index,int end,vector<int> &nums,vector<int> &dp){
-    if(index>end) return 0;
-    if(dp[index]!=-1) return dp[index];
-    int nottake=0+func(index+1,end,nums,dp);
-    int take=nums[index]+func(index+2,end,nums,dp);
-    return dp[index]=max(nottake,take);
-}
     int rob(vector<int>& nums) {
         int n=nums.size();
         if(n==1) return nums[0];
-        vector<int> dp1(n,-1);
-        vector<int> dp2(n,-1);
-        int first=func(0,n-2,nums,dp1);
-        int second=func(1,n-1,nums,dp2);
+        vector<int> dp1(n+2,0);
+        vector<int> dp2(n+2,0);
+        for(int index=n-2;index>=0;index--){
+            int nottake=0+dp1[index+1];
+            int take=nums[index]+dp1[index+2];
+            dp1[index]=max(nottake,take);
+        }
+        int first=dp1[0];
+        for(int index=n-1;index>=1;index--){
+            int nottake=0+dp2[index+1];
+            int take=nums[index]+dp2[index+2];
+            dp2[index]=max(nottake,take);
+        }
+        int second=dp2[1];
         return max(first,second);
     }
 };
